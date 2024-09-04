cb69ff53bf27a73ae7b0bf58b125f6efbebd9782
noahpy <noschl@proton.me>
2024-09-04 12:02:47 +0200

    read deliveryStart / End, product

--- src/epexlib/utils/csv.cpp
+++ src/epexlib/utils/csv.cpp
@@ -1,5 +1,6 @@
 
 #include "epexlib/utils/csv.hpp"
+#include "epexlib/utils/utils.hpp"
 #include <fstream>
 
 namespace epexlib {
@@ -19,15 +20,28 @@ EventData loadEventDataFromCSV(const std::string& filename, bool ignoreFirstLine
 
     std::vector<EventDataRow> rows;
 
+    std::string firstLine = "";
+
     while (std::getline(file, line)) {
+        if (firstLine == "") {
+            firstLine = line;
+        }
         EventDataRow row = EventDataRow::rowFromLine(line);
         rows.push_back(row);
     }
 
-    std::chrono::system_clock::time_point deliveryStart;
-    std::chrono::system_clock::time_point deliveryEnd;
+    // read deliveryTimes from first line
+    size_t pos = findNthChar(firstLine, ',', 6);
+    size_t pos2 = firstLine.find(',', pos + 1);
+    size_t pos3 = firstLine.find(',', pos2 + 1);
 
-    std::string product;
+    std::string substr1 = firstLine.substr(pos + 1, pos2 - pos - 1);
+    std::string substr2 = firstLine.substr(pos2 + 1, pos3 - pos2 - 1);
+
+    std::chrono::system_clock::time_point deliveryStart = stringToTimePoint(substr1);
+    std::chrono::system_clock::time_point deliveryEnd = stringToTimePoint(substr2);
+
+    std::string product = productFromDelivery(deliveryStart, deliveryEnd);
 
     return EventData(deliveryStart, deliveryEnd, product, rows);
 };
--- src/epexlib/utils/utils.hpp
+++ src/epexlib/utils/utils.hpp
@@ -28,4 +28,23 @@ inline std::chrono::system_clock::time_point stringToTimePoint(const std::string
     return tp;
 }
 
+inline size_t findNthChar(const std::string& s, char c, size_t n)
+{
+    size_t pos = 0;
+    for (size_t i = 0; i < n; ++i) {
+        pos = s.find(c, pos + 1);
+    }
+    return pos;
+}
+
+inline std::string productFromDelivery(
+    std::chrono::system_clock::time_point deliveryStart,
+    std::chrono::system_clock::time_point deliveryEnd)
+{
+    // calculate difference in time in minutes
+    auto duration = std::chrono::duration_cast<std::chrono::minutes>(deliveryEnd - deliveryStart);
+    std::string product = std::to_string(duration.count()) + "m";
+    return product;
+}
+
 }
